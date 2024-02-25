4415333dd072000115c5d291e419d3e993f3136d
Vadym Usenko <usencko.vadim@gmail.com>
2024-02-25 21:00:40 +0200

    Add observer pattern

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -8,6 +8,7 @@
 #include <algorithm>
 #include <string>
 #include <numeric>
+#include <memory>
 
 
 class Weapon {
@@ -121,6 +122,40 @@ private:
 };
 
 
+class Observer {
+public:
+	virtual void update(float damage) = 0;
+};
+
+
+
+class Enemy : public Observer {
+public:
+	Enemy(const std::string& _name, float _health) : name(_name), health(_health) {}
+	
+	void update(float damage) {
+		if (damage - health < 0) {
+			health -= damage;
+			std::cout << name << " has received " << damage << " damage and now has " << health << " HP" << std::endl;
+		}
+		else std::cout << name << " has 0 HP!" << std::endl;
+	}
+
+	std::string getName() {
+		return name;
+	}
+
+	
+
+	bool operator==(const Enemy& enemy) {
+		return this->name == enemy.name;
+	}
+
+private:
+	std::string name;
+	float health;
+};
+
 
 class Character {
 public:
@@ -149,56 +184,45 @@ public:
 
 	std::string getName() {
 		std::cout << "This character called " << name << std::endl;
+		return name;
 	}
+
+	void addEnemyInRange(Enemy& enemy) {
+		enemiesInRange.push_back(enemy);
+	}
+
+	void deleteEnemyInRange(Enemy& enemy) {
+		std::vector<Enemy>::iterator position = std::find(enemiesInRange.begin(), enemiesInRange.end(), enemy);
+	}
+
+	void dealDamage() {
+		for (Enemy& enemy : enemiesInRange) {
+			enemy.update(GetModifiedDamage());
+		}
+	}
+
+
 
 private:
 	std::unique_ptr <DamageModifier> modifier;
 	std::unique_ptr <Weapon> weapon;
 	std::string name;
+	std::vector <Enemy> enemiesInRange;
 	float enemyHealth;
 };
+
+
 
 int ParityDamageModifier::counter = 0;
 
 int main() {
-	
+	Enemy* cobald = new Enemy("Rex", 100);
+	Enemy* harpy = new Enemy("Ana", 100);
 
-	std::unique_ptr <Character> druid = std::make_unique <Character>
-		(std::make_unique <ParityDamageModifier> (1.25), std::make_unique <MeleeWeapon> (25, "claws"), "Lone Druid", 100);
+	Character* mainHero = new Character(std::make_unique<MultiplicationDamageModifier>(1.25), std::make_unique <MeleeWeapon>(100, "Frostmourne"), "Arthas", 200);
 
-	druid->GetModifiedDamage();
+	mainHero->addEnemyInRange(*cobald);
+	mainHero->addEnemyInRange(*harpy);
 
-	std::cout << ParityDamageModifier::counter;
-	druid->GetModifiedDamage();
-
-	std::cout << ParityDamageModifier::counter;
-	druid->GetModifiedDamage();
-
-	
-
-	std::unique_ptr <Character> sorcerer = std::make_unique <Character> (std::make_unique <AdditionDamageModifier> (25), std::make_unique <MagicalRangedWeapon> (10, "rog"), "Crystal Maiden", 100);
-	sorcerer->GetModifiedDamage();
-
-
-	std::unique_ptr <DamageModifier> modifier = nullptr;
-	std::unique_ptr <Character> soldier = std::make_unique <Character> (nullptr, std::make_unique <RangedWeapon> (35, "bow"), "Bob", 100);
-	int num;
-	std::cout << "Select modifier 1: Multiplication; 2: Addition; 3: Parity" << std::endl;
-	while (std::cin >> num) {
-		switch (num) {
-			case 1:
-				soldier->setModifier(std::make_unique <MultiplicationDamageModifier> (1.2));
-				soldier->GetModifiedDamage();
-				break;
-			case 2:
-				soldier->setModifier(std::make_unique <AdditionDamageModifier> (10));
-				soldier->GetModifiedDamage();
-				break;
-			case 3:
-				soldier->setModifier(std::make_unique <ParityDamageModifier>(1.5));
-				soldier->GetModifiedDamage();
-				break;
-		}
-		std::cout << "Select modifier 1: Multiplication; 2: Addition; 3: Parity" << std::endl;
-	}
+	mainHero->dealDamage();
 }
